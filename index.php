7f2a860ddf3301e6e2df52e213ffc5f0fa958d6b
tiencahi chamnan <kanompang962@gmail.com>
2022-12-02 00:40:16 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD_PDO</title>
     <link rel="stylesheet" href="bootstrap/bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
 <div class="container">
