bef25324a1c476cc779cc42a239b97151569ff46
tiencahi chamnan <kanompang962@gmail.com>
2022-12-02 00:18:30 +0700

    index

--- index.php
+++ index.php
@@ -8,8 +8,44 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD_PDO</title>
+    <link rel="stylesheet" href="bootstrap/bootstrap.css">
 </head>
 <body>
-    <h1>Hello Word!</h1>
+<div class="container">
+    <div class="display-3 text-center">Information</div>
+    <a href="add.php" class="btn btn-success mb-3">Add+</a>
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+            <tr>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Edit Name</th>
+                <th>Delete</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php 
+                $select_stmt = $db->prepare("SELECT * FROM user");
+                $select_stmt->execute();
+
+                while ($row = $select_stmt->fetch(PDO::FETCH_ASSOC)) {
+            ?>
+
+                <tr>
+                    <td><?php echo $row["username"]; ?></td>
+                    <td><?php echo $row["password"]; ?></td>
+                    <td><a href="edit.php?update_id=<?php echo $row["id"]; ?>" class="btn btn-warning">Edit</a></td>
+                    <td><a href="?delete_id=<?php echo $row["id"]; ?>" class="btn btn-danger">Delete</a></td>
+                </tr>
+
+            <?php } ?>
+        </tbody>
+    </table>
+    </div>
+    
+    <script src="js/slim.js"></script>
+    <script src="js/popper.js"></script>
+    <script src="js/bootstrap.js"></script>
 </body>
 </html>
